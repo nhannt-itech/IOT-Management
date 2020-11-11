b8894d2ba087b4d991166801e5d898db1bd238a6
Nguyen Truong <nguyentruong.120300@gmail.com>
2020-11-11 11:50:05 +0700

    Add Television CRUD officals

---
+++ IOTManagementGroup7.Models/Television.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace IOTManagementGroup7.Models
+{
+    public class Television
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool PowerStatus { get; set; }
+        public string CurrentChannel { get; set; }
+        public bool RecordingStatus { get; set; }
+        [Range(0, 100)]
+        public int Volume { get; set; }
+        public string SourceCode { get; set; }
+        [Required]
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
