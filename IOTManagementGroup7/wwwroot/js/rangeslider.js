71d9f34c066268af7487f09f9f08565e418e3045
nhannt-itech <67116951+nhannt-itech@users.noreply.github.com>
2020-11-30 22:47:50 +0700

    rangeslider for upsert sensor

--- IOTManagementGroup7/wwwroot/js/rangeslider.js
+++ IOTManagementGroup7/wwwroot/js/rangeslider.js
@@ -1,7 +1,7 @@
-﻿//var slider = document.getElementById("myRange");
-//var output = document.getElementById("demo");
-//output.innerHTML = slider.value;
+﻿var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
 
-//slider.oninput = function () {
-//    output.innerHTML = this.value;
-//}
+slider.oninput = function () {
+    output.innerHTML = this.value;
+}
