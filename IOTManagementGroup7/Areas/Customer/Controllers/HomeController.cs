057c2255a794236ea2bf2b99401f9b0cb9a1b747
nhannt-itech <67116951+nhannt-itech@users.noreply.github.com>
2020-12-22 15:01:23 +0700

    chart in home index

--- IOTManagementGroup7/Areas/Customer/Controllers/HomeController.cs
+++ IOTManagementGroup7/Areas/Customer/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using IOTManagementGroup7.Models;
+using IOTManagementGroup7.DataAccess.Repository.IRepository;
 
 namespace IOTManagementGroup7.Controllers
 {
@@ -13,10 +14,12 @@ namespace IOTManagementGroup7.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
@@ -33,6 +36,40 @@ namespace IOTManagementGroup7.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public IActionResult LoadBarChart()
+        {
+            var index = 0;
+            var allObjDeviceType = _unitOfWork.DeviceType.GetAll();
+            string[] Labels = new string[allObjDeviceType.Count()];
+            string[] Values = new string[allObjDeviceType.Count()];
+
+            foreach (var item in allObjDeviceType)
+            {
+                Labels[index] = item.Name;
+                Values[index] = _unitOfWork.Device.GetAll().Count(x => x.DeviceTypeId == item.Id).ToString();
+                index++;
+            }
+            return Json(new { labels = Labels, values = Values });
+        }
+
+        [HttpGet]
+        public IActionResult LoadCard()
+        {
+            int NumUsers = _unitOfWork.ApplicationUser.GetAll().Count();
+            int NumProjects = _unitOfWork.Project.GetAll().Count();
+            int NumDeviceTypes = _unitOfWork.DeviceType.GetAll().Count();
+            int NumDevices = _unitOfWork.Device.GetAll().Count();
+
+            return Json(new
+            {
+                numUsers = NumUsers,
+                numProjects = NumProjects,
+                numDeviceTypes = NumDeviceTypes,
+                numDevices = NumDevices
+            });
         }
     }
 }
